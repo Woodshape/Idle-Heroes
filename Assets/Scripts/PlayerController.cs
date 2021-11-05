b43e09479a9a4448c4a5d6fbcd88633634136270
Woodshape <rkarschny@gmail.com>
2021-11-05 17:52:30 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void FixedUpdate() {
+        throw new NotImplementedException();
+    }
+}
